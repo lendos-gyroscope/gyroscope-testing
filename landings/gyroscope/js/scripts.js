e357922bba232ce8979a09d18eabb24136d4f574
Ruslan Developer <kbt3.corei7@gmail.com>
2020-11-08 12:10:51 +0300

    Create project ver. 1.0.1

---
+++ landings/gyroscope/js/scripts.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$(function () {
+    var requestUri = document.location.pathname;
+    var requestParams = document.location.search;
+
+    console.log("requestUri: " + requestUri );
+    console.log("requestParams: " + requestParams);
+});
